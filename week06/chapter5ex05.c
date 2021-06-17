6a3986fb624da6947509130bf51768db5385508c
Catunima <2009055@upy.edu.mx>
2021-06-17 11:54:12 -0500

    delete more

---
+++ week06/chapter5ex05.c
@@ -0,0 +1,17 @@
+/*
+Author: Eduardo Flores
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void){
+
+    float centi = 0, fahren = 0;//la variable centi es la que recibira el dato par convertir
+
+    printf("");
+
+
+    system("pause");
+    return 0;
+}
