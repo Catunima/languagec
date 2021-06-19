167d8e56a6985e31849425fffae8127ddbae310b
Catunima <2009055@upy.edu.mx>
2021-06-19 10:30:16 -0500

    ex2-6

---
+++ week07/chapter6ex02.c
@@ -0,0 +1,31 @@
+/*
+Author: Eduardo Flores
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+
+    float grade = 0;
+
+    printf("Please, enter your grade: ");
+    scanf("%f", &grade);
+
+    if(grade >= 0 && grade <=60){
+        printf("F\n");
+    } else if(grade >=61 && grade <=70){
+        printf("D\n");
+    }else if (grade >=71 && grade <=80) 
+    {
+        printf("C\n");
+    }else if(grade >=81 && grade <= 90){
+        printf("B\n");
+    }else if(grade >= 91 && grade <= 100){
+        printf("A\n");
+    }
+    
+
+    system("pause");
+    return 0;
+}
