d5a04db6808efa464b3415445ddf52cf0b1347cc
Catunima <2009055@upy.edu.mx>
2021-07-04 12:47:01 -0500

    ex2

--- week8/chapter9ex02.c
+++ week8/chapter9ex02.c
@@ -1,7 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
 
-int main(void){
+#include <stdio.h>
 
-    return 0;
+int begins(char string1[], char string2[]) {
+	int i = 0;          /* index into a string */
+
+	while (1) {
+
+		if (string1[ i ] == '\0') {
+			return(1);
+		}
+		if (string1[i] != string2[i]) {
+			return(0);
+		}
+
+		++i;
+	}
+}
+
+int main(void) {
+
+	if (begins("fodo", "foobar")) {
+		printf("this okay!\n");
+	} else {
+		printf("no.\n");
+	}
+
+	return(0);
 }
